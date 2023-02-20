7ef06902314b10e954c6019aa444dd3810b08290
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-02-20 19:41:09 +0700

    feat: VAL-3 | create login register api and routes

--- internal/controllers/user_controller.go
+++ internal/controllers/user_controller.go
@@ -1,14 +1,18 @@
 package controllers
 
 import (
+	"time"
+
 	"github.com/MangriMen/Value-Back/api/database"
+	"github.com/MangriMen/Value-Back/internal/helpers"
+	"github.com/MangriMen/Value-Back/internal/models"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"github.com/samber/lo"
 )
 
 func GetUsers(c *fiber.Ctx) error {
 	db, err := database.OpenDBConnection()
-
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":   true,
@@ -17,7 +21,6 @@ func GetUsers(c *fiber.Ctx) error {
 	}
 
 	users, err := db.GetUsers()
-
 	if err != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error":   true,
@@ -26,6 +29,11 @@ func GetUsers(c *fiber.Ctx) error {
 			"users":   nil,
 		})
 	}
+
+	users = lo.Map(users, func(item models.User, index int) models.User {
+		item.PrepareToSend()
+		return item
+	})
 
 	return c.JSON(fiber.Map{
 		"error":   false,
@@ -37,7 +45,6 @@ func GetUsers(c *fiber.Ctx) error {
 
 func GetUser(c *fiber.Ctx) error {
 	id, err := uuid.Parse(c.Params("id"))
-
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":   true,
@@ -46,7 +53,6 @@ func GetUser(c *fiber.Ctx) error {
 	}
 
 	db, err := database.OpenDBConnection()
-
 	if err != nil {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"error":   true,
@@ -55,7 +61,6 @@ func GetUser(c *fiber.Ctx) error {
 	}
 
 	user, err := db.GetUser(id)
-
 	if err != nil {
 		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
 			"error":   true,
@@ -64,9 +69,234 @@ func GetUser(c *fiber.Ctx) error {
 		})
 	}
 
+	user.PrepareToSend()
 	return c.JSON(fiber.Map{
 		"error":   false,
 		"message": nil,
 		"user":    user,
 	})
+}
+
+func LoginUser(c *fiber.Ctx) error {
+	user := &models.User{}
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	foundUser, err := db.GetUserByEmail(user.Email)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error":   true,
+			"message": "user with this email not found",
+		})
+	}
+
+	if ok := helpers.CheckPasswordHash(user.Password, foundUser.Password); !ok {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error":   true,
+			"message": "wrong password",
+		})
+	}
+
+	token, err := helpers.GenerateNewAccessToken()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	foundUser.PrepareToSend()
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": nil,
+		"token":   token,
+		"user":    foundUser,
+	})
+}
+
+func CreateUser(c *fiber.Ctx) error {
+	user := &models.User{}
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	user.Id = uuid.New()
+	user.CreatedAt = time.Now()
+	user.UpdatedAt = user.CreatedAt
+
+	validate := helpers.NewValidator()
+	if err := validate.Struct(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": helpers.ValidatorErrors(err),
+		})
+	}
+
+	user.Password, err = helpers.HashPassword(user.Password)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": "cannot create user",
+		})
+	}
+
+	if err := db.CreateUser(user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	token, err := helpers.GenerateNewAccessToken()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	user.PrepareToSend()
+	return c.JSON(fiber.Map{
+		"error":   false,
+		"message": nil,
+		"token":   token,
+		"user":    user,
+	})
+}
+
+func UpdateUser(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	claims, err := helpers.ExtractTokenMetadata(c)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	if claims.Expires < now {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error":   true,
+			"message": "unathorized, check expiration time of your token",
+		})
+	}
+
+	user := &models.User{}
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	foundUser, err := db.GetUser(user.Id)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error":   true,
+			"message": "user with this ID not found",
+		})
+	}
+
+	user.UpdatedAt = time.Now()
+
+	validate := helpers.NewValidator()
+	if err := validate.Struct(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": helpers.ValidatorErrors(err),
+		})
+	}
+
+	if err := db.UpdateUser(foundUser.Id, user); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"erorr":   true,
+			"message": err.Error(),
+		})
+	}
+
+	return c.SendStatus(fiber.StatusCreated)
+}
+
+func DeleteUser(c *fiber.Ctx) error {
+	now := time.Now().Unix()
+
+	claims, err := helpers.ExtractTokenMetadata(c)
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	if claims.Expires < now {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error":   true,
+			"message": "unathorized, check expiration time of your token",
+		})
+	}
+
+	user := &models.User{}
+	if err := c.BodyParser(user); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	validate := helpers.NewValidator()
+	if err := validate.StructPartial(user, "id"); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": helpers.ValidatorErrors(err),
+		})
+	}
+
+	db, err := database.OpenDBConnection()
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"erorr":   true,
+			"message": "book with this ID not found",
+		})
+	}
+
+	if err := db.DeleteUser(user.Id); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": err.Error(),
+		})
+	}
+
+	return c.SendStatus(fiber.StatusNoContent)
 }
--- internal/helpers/start_server.go
+++ internal/helpers/start_server.go
@@ -9,29 +9,28 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
-// StartServerWithGracefulShutdown function for starting server with a graceful shutdown.
+// Starting server with a graceful shutdown
 func StartServerWithGracefulShutdown(a *fiber.App) {
-	// Create channel for idle connections.
-	idleConnsClosed := make(chan struct{})
+	idleConnectionsClosed := make(chan struct{})
 
 	go func() {
 		sigint := make(chan os.Signal, 1)
-		signal.Notify(sigint, os.Interrupt) // Catch OS signals.
+		signal.Notify(sigint, os.Interrupt) // Catch OS signals
 		<-sigint
 
-		// Received an interrupt signal, shutdown.
+		// Received an interrupt signal, shutdown
 		if err := a.Shutdown(); err != nil {
 			// Error from closing listeners, or context timeout:
-			log.Printf("Oops... Server is not shutting down! Reason: %v", err)
+			log.Printf("Oops... Server cannot be shutted down! Reason: %v", err)
 		}
 
-		close(idleConnsClosed)
+		close(idleConnectionsClosed)
 	}()
 
-	// Run server.
+	// Run server
 	if err := a.Listen(fmt.Sprintf(":%s", os.Getenv("SERVER_PORT"))); err != nil {
-		log.Printf("Oops... Server is not running! Reason: %v", err)
+		log.Printf("Oops... Server cannot be started! Reason: %v", err)
 	}
 
-	<-idleConnsClosed
+	<-idleConnectionsClosed
 }
