b483d6951f067515980033a05288b82966be6935
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-03-21 10:52:44 +0700

    feat: DB-26 | move avatar to user table; add triggers; add view (#27)

--- scripts/postgres-initd/init_tables.sql
+++ scripts/postgres-initd/init_tables.sql
@@ -3,7 +3,9 @@
 --
 
 -- Dumped from database version 14.5
--- Dumped by pg_dump version 14.5
+-- Dumped by pg_dump version 14.6
+
+-- Started on 2023-03-11 14:00:43 UTC
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -16,11 +18,59 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
+--
+-- TOC entry 2 (class 3079 OID 16385)
+-- Name: pg_stat_statements; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS pg_stat_statements WITH SCHEMA public;
+
+
+--
+-- TOC entry 3361 (class 0 OID 0)
+-- Dependencies: 2
+-- Name: EXTENSION pg_stat_statements; Type: COMMENT; Schema: -; Owner:
+--
+
+COMMENT ON EXTENSION pg_stat_statements IS 'track planning and execution statistics of all SQL statements executed';
+
+
+--
+-- TOC entry 216 (class 1255 OID 16410)
+-- Name: add_user_info(); Type: FUNCTION; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
+--
+
+CREATE FUNCTION public.add_user_info() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$BEGIN
+	INSERT INTO user_info VALUES (NEW.id, null);
+	RETURN NEW;
+END;$$;
+
+
+ALTER FUNCTION public.add_user_info() OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
+
+--
+-- TOC entry 217 (class 1255 OID 16411)
+-- Name: remove_user_info(); Type: FUNCTION; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
+--
+
+CREATE FUNCTION public.remove_user_info() RETURNS trigger
+    LANGUAGE plpgsql
+    AS $$BEGIN
+	DELETE FROM user_info WHERE id = NEW.id;
+	RETURN NEW;
+END;$$;
+
+
+ALTER FUNCTION public.remove_user_info() OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
+
 SET default_tablespace = '';
 
 SET default_table_access_method = heap;
 
 --
+-- TOC entry 212 (class 1259 OID 16412)
 -- Name: posts; Type: TABLE; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -34,20 +84,20 @@ CREATE TABLE public.posts (
 ALTER TABLE public.posts OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
 
 --
+-- TOC entry 213 (class 1259 OID 16417)
 -- Name: user_info; Type: TABLE; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
 CREATE TABLE public.user_info (
     id uuid NOT NULL,
-    user_id uuid NOT NULL,
-    about character varying(256),
-    avatar_url text
+    about character varying(256)
 );
 
 
 ALTER TABLE public.user_info OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
 
 --
+-- TOC entry 214 (class 1259 OID 16420)
 -- Name: users; Type: TABLE; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -58,13 +108,36 @@ CREATE TABLE public.users (
     username character varying(32) NOT NULL,
     name character varying(32),
     created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL
+    updated_at timestamp with time zone NOT NULL,
+    avatar_url text
 );
 
 
 ALTER TABLE public.users OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
 
 --
+-- TOC entry 215 (class 1259 OID 16425)
+-- Name: user_full_info; Type: VIEW; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
+--
+
+CREATE VIEW public.user_full_info AS
+ SELECT users.id,
+    users.email,
+    users.password,
+    users.username,
+    users.name,
+    users.created_at,
+    users.updated_at,
+    users.avatar_url,
+    user_info.about
+   FROM (public.users
+     LEFT JOIN public.user_info USING (id));
+
+
+ALTER TABLE public.user_full_info OWNER TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
+
+--
+-- TOC entry 3205 (class 2606 OID 16430)
 -- Name: posts posts_pkey; Type: CONSTRAINT; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -73,6 +146,7 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- TOC entry 3207 (class 2606 OID 16446)
 -- Name: user_info user_info_pkey; Type: CONSTRAINT; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -81,6 +155,7 @@ ALTER TABLE ONLY public.user_info
 
 
 --
+-- TOC entry 3209 (class 2606 OID 16432)
 -- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -89,6 +164,23 @@ ALTER TABLE ONLY public.users
 
 
 --
+-- TOC entry 3213 (class 2620 OID 16433)
+-- Name: users add_user_info_trigger; Type: TRIGGER; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
+--
+
+CREATE TRIGGER add_user_info_trigger AFTER INSERT ON public.users FOR EACH ROW EXECUTE FUNCTION public.add_user_info();
+
+
+--
+-- TOC entry 3212 (class 2620 OID 16434)
+-- Name: users remove_user_info_trigger; Type: TRIGGER; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
+--
+
+CREATE TRIGGER remove_user_info_trigger AFTER DELETE ON public.users FOR EACH ROW EXECUTE FUNCTION public.remove_user_info();
+
+
+--
+-- TOC entry 3210 (class 2606 OID 16435)
 -- Name: posts posts_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
@@ -97,21 +189,15 @@ ALTER TABLE ONLY public.posts
 
 
 --
+-- TOC entry 3211 (class 2606 OID 16440)
 -- Name: user_info user_info_user_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
 --
 
 ALTER TABLE ONLY public.user_info
-    ADD CONSTRAINT user_info_user_id_fkey FOREIGN KEY (user_id) REFERENCES public.users(id);
+    ADD CONSTRAINT user_info_user_id_fkey FOREIGN KEY (id) REFERENCES public.users(id);
 
 
---
--- Name: SCHEMA public; Type: ACL; Schema: -; Owner: ea1fb999-4aab-4142-9101-facdc7d5b83b
---
-
-REVOKE ALL ON SCHEMA public FROM PUBLIC;
-GRANT ALL ON SCHEMA public TO "ea1fb999-4aab-4142-9101-facdc7d5b83b";
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
+-- Completed on 2023-03-11 14:00:43 UTC
 
 --
 -- PostgreSQL database dump complete
