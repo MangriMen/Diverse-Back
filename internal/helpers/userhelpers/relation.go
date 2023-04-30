e8fc525c6c3ad54c244a1b2ab36c17b4db7f2164
MangriMen <49300253+MangriMen@users.noreply.github.com>
2023-04-30 11:59:14 +0700

    fix: DB-65 | fix relations routes (#67)

--- internal/helpers/userhelpers/relation.go
+++ internal/helpers/userhelpers/relation.go
@@ -2,8 +2,12 @@
 package userhelpers
 
 import (
+	"time"
+
 	"github.com/MangriMen/Diverse-Back/api/database"
 	"github.com/MangriMen/Diverse-Back/internal/models"
+	"github.com/MangriMen/Diverse-Back/internal/parameters"
+	"github.com/google/uuid"
 )
 
 // PrepareRelationToSend prepares a relation object for sending by fetching additional data from the database
@@ -22,12 +26,50 @@ func PrepareRelationToSend(relation models.DBRelation, db *database.Queries) *mo
 
 // PrepareRelationStatusToSend transforms the list of relations for the user
 // into a structure reflecting their status.
-func PrepareRelationStatusToSend(relationStatus []models.DBRelation) map[models.RelationType]bool {
+func PrepareRelationStatusToSend(userID uuid.UUID, relationStatus []models.DBRelation) map[models.RelationType]bool {
 	preparedStatus := make(map[models.RelationType]bool)
 
 	for _, relation := range relationStatus {
-		preparedStatus[relation.Type] = true
+		if relation.Type != models.Blocked && userID == relation.RelationUserID {
+			preparedStatus[models.Follower] = true
+		} else {
+			preparedStatus[relation.Type] = true
+		}
 	}
 
 	return preparedStatus
+}
+
+// AddRelation adds relation.
+func AddRelation(
+	params *parameters.RelationGetStatusParams,
+	query *parameters.RelationAddDeleteRequestQuery,
+	db *database.Queries) error {
+	relation := &models.DBRelation{
+		BaseRelation: models.BaseRelation{
+			ID:        uuid.New(),
+			Type:      query.Type,
+			CreatedAt: time.Now(),
+		},
+		UserID:         params.User,
+		RelationUserID: params.RelationUser,
+	}
+
+	if err := db.AddRelation(relation); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// DeleteRealtionWithReverse adds two relation reverse to each other.
+func DeleteRealtionWithReverse(
+	params *parameters.RelationGetStatusParams,
+	query *parameters.RelationAddDeleteRequestQuery,
+	db *database.Queries) error {
+	if err := db.DeleteRelation(params, query); err != nil {
+		return err
+	}
+
+	return nil
 }
